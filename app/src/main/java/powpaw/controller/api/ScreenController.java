60c79946cdf91cbe96429230c3aca0b58b4ff799
Giacomo Grassetti <giacomo.grassetti@studio.unibo.it>
2023-03-09 16:49:31 +0100

    add controller for MVC

---
+++ app/src/main/java/powpaw/controller/api/ScreenController.java
@@ -0,0 +1,10 @@
+package powpaw.controller.api;
+
+public class ScreenController {
+    public static final int SIZE_HD_W = 1280;
+    public static final int SIZE_HD_H = 720;
+
+    public void setScreenSize(int width, int height){
+        //TODO
+    }
+}
