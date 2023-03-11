16d1dce010e9f2056e5d78fdeb8de2b31d73c1b7
Simone Collorà <simone.collora@studio.unibo.it>
2023-03-11 11:03:01 +0100

    Refactoring

--- app/src/main/java/powpaw/view/api/StartMenu.java
+++ app/src/main/java/powpaw/view/api/StartMenu.java
@@ -4,28 +4,33 @@ import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import powpaw.App;
 
 public class StartMenu extends GridPane{
     private Button start;
     private Button exit;
 
-    public StartMenu(GridPane root){
-        root.setAlignment(Pos.CENTER);
+    public StartMenu(){
+        setAlignment(Pos.CENTER);
         start = new Button("START");
         exit = new Button("EXIT");
         setVgap(15);
         setHgap(15);
-        start.prefWidthProperty().bind(root.widthProperty().divide(4));
-        start.prefHeightProperty().bind(root.heightProperty().divide(4));
-        exit.prefWidthProperty().bind(root.widthProperty().divide(4));
-        exit.prefHeightProperty().bind(root.heightProperty().divide(4));
+        start.prefWidthProperty().bind(widthProperty().divide(4));
+        start.prefHeightProperty().bind(heightProperty().divide(4));
+        exit.prefWidthProperty().bind(widthProperty().divide(4));
+        exit.prefHeightProperty().bind(heightProperty().divide(4));
         start.setMaxSize(300, 70);
         exit.setMaxSize(300, 70);
         add(start,0,0);
         add(exit,0,1);
         start.setOnAction(e->{
-           /* App app = new App();
-            start.getScene().setRoot(app.getGridPane());*/
+            try{
+                App app = new App();
+                app.getGame();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
         });
         exit.setOnAction(e->{
             Platform.exit();
