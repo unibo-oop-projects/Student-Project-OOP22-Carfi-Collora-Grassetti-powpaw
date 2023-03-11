416ada57ec5d0bc73a55d6dcf379f9031f4c4e0b
Simone Collorà <simone.collora@studio.unibo.it>
2023-03-11 16:09:41 +0100

    Added pause button

--- app/src/main/java/powpaw/view/api/GameInterface.java
+++ app/src/main/java/powpaw/view/api/GameInterface.java
@@ -1,33 +1,37 @@
 package powpaw.view.api;
 
 import javafx.geometry.Pos;
-import javafx.scene.input.KeyCode;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
-public class GameInterface extends StackPane{
-    private int damageP1=0;
-    private int damageP2=0;
+public class GameInterface extends StackPane {
+    private int damageP1 = 0;
+    private int damageP2 = 0;
     private Text damP1;
     private Text damP2;
+    private Button pause;
 
-
-    public GameInterface(){
-        damP1 = new Text(0,0,"P1: " + String.valueOf(damageP1));
-        damP2 = new Text(0,0,"P2: " + String.valueOf(damageP2));
+    public GameInterface() {
+        damP1 = new Text(0, 0, "P1: " + String.valueOf(damageP1));
+        damP2 = new Text(0, 0, "P2: " + String.valueOf(damageP2));
+        pause = new Button("X");
+        pause.prefWidthProperty().bind(widthProperty().divide(4));
+        pause.prefHeightProperty().bind(heightProperty().divide(4));
+        pause.setMaxSize(100, 70);
         damP1.setStyle("-fx-font: 50 arial;");
         damP2.setStyle("-fx-font: 50 arial;");
         damP1.setFill(Color.RED);
         setPrefSize(400, 400);
         getChildren().add(damP1);
         getChildren().add(damP2);
-        setAlignment(damP1,Pos.TOP_LEFT);
-        setAlignment(damP2,Pos.TOP_RIGHT);
-        setOnKeyTyped(ke ->{
-            if(ke.getCode()==KeyCode.SPACE){
-                damageP1+= 4;
-            }
+        getChildren().add(pause);
+        setAlignment(damP1, Pos.TOP_LEFT);
+        setAlignment(damP2, Pos.TOP_RIGHT);
+        setAlignment(pause, Pos.TOP_CENTER);
+        pause.setOnAction(e ->{
+            pause.getScene().setRoot(new PauseMenu());
         });
     }
 }
