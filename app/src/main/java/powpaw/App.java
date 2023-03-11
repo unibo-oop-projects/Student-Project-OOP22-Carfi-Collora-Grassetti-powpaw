8676a5a797369656b140449d5f53b5a1015991bd
Simone Collorà <simone.collora@studio.unibo.it>
2023-03-11 11:04:42 +0100

    GameInterface added

--- app/src/main/java/powpaw/App.java
+++ app/src/main/java/powpaw/App.java
@@ -5,14 +5,20 @@ package powpaw;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import powpaw.model.api.GameInterface;
 import powpaw.view.api.StartMenu;
 
 public class App extends Application {
-    StartMenu menu;
-    private GridPane root;
-    Scene scene;
+    private StartMenu menu;
+    private Scene sceneMenu;
+    private Scene sceneGame;
+    private final Stage stage;
+    private GameInterface game;
+
+    public App(){
+        stage = new Stage();
+    }
 
     public static void main(String[] args) {
         Application.launch(App.class, args);
@@ -20,14 +26,25 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        root = new GridPane();
-        menu = new StartMenu(root);
-        root.getChildren().add(menu);
-        Scene scene = new Scene(root,500,300);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        getMenu();
     }
-    public GridPane getGridPane(){
-        return root;
+
+    public void getGame() throws Exception {
+        this.stage.close();
+        game = new GameInterface();
+        sceneGame = new Scene(game, 500, 300);
+        this.stage.setScene(sceneGame);
+        this.stage.setTitle("PowPaw");
+        this.stage.show();
+    }
+
+    public void getMenu() {
+        menu = new StartMenu();
+        sceneMenu = new Scene(menu, 500, 300);
+        this.stage.setScene(sceneMenu);
+        this.stage.show();
+    }
+    public Stage getStage(){
+        return this.stage;
     }
 }
---
+++ app/src/main/java/powpaw/model/api/GameInterface.java
@@ -0,0 +1,33 @@
+package powpaw.model.api;
+
+import javafx.geometry.Pos;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+public class GameInterface extends StackPane{
+    private int damageP1=0;
+    private int damageP2=0;
+    private Text damP1;
+    private Text damP2;
+
+
+    public GameInterface(){
+        damP1 = new Text(0,0,"P1: " + String.valueOf(damageP1));
+        damP2 = new Text(0,0,"P2: " + String.valueOf(damageP2));
+        damP1.setStyle("-fx-font: 50 arial;");
+        damP2.setStyle("-fx-font: 50 arial;");
+        damP1.setFill(Color.RED);
+        setPrefSize(400, 400);
+        getChildren().add(damP1);
+        getChildren().add(damP2);
+        setAlignment(damP1,Pos.TOP_LEFT);
+        setAlignment(damP2,Pos.TOP_RIGHT);
+        setOnKeyTyped(ke ->{
+            if(ke.getCode()==KeyCode.SPACE){
+                damageP1+= 4;
+            }
+        });
+    }
+}
