3c047218cf862779774b3e40aad809a1f8c9fe0c
Simone Collorà <simone.collora@studio.unibo.it>
2023-03-20 11:39:11 +0100

    Pause Menu removed

--- app/src/main/java/powpaw/view/api/GameInterface.java
+++ app/src/main/java/powpaw/view/api/GameInterface.java
@@ -1,7 +1,6 @@
 package powpaw.view.api;
 
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -11,25 +10,16 @@ public class GameInterface extends StackPane {
     private double damageP2 = 0.0;
     private Text damP1;
     private Text damP2;
-    private final Button pause;
 
     public GameInterface() {
         damP1 = new Text(0, 0, "P1: " + String.valueOf(damageP1));
         damP2 = new Text(0, 0, "P2: " + String.valueOf(damageP2));
-        pause = new Button("X");
-        pause.prefWidthProperty().bind(widthProperty().divide(4));
-        pause.prefHeightProperty().bind(heightProperty().divide(4));
-        pause.setMaxSize(100, 70);
         damP1.setStyle("-fx-font: 50 arial;");
         damP2.setStyle("-fx-font: 50 arial;");
         damP1.setFill(Color.RED);
         setPrefSize(400, 400);
-        getChildren().addAll(damP1, damP2, pause);
+        getChildren().addAll(damP1, damP2);
         setAlignment(damP1, Pos.TOP_LEFT);
         setAlignment(damP2, Pos.TOP_RIGHT);
-        setAlignment(pause, Pos.TOP_CENTER);
-        pause.setOnAction(e -> {
-            pause.getScene().setRoot(new PauseMenu());
-        });
     }
 }
