00b011f1a36350f7d463c14251d9bf934277a2af
Simone Collorà <simone.collora@studio.unibo.it>
2023-03-21 12:35:18 +0100

    StatsController created

---
+++ app/src/main/java/powpaw/controller/impl/StatsControllerImpl.java
@@ -0,0 +1,70 @@
+package powpaw.controller.impl;
+
+import powpaw.controller.api.StatsController;
+
+public class StatsControllerImpl implements StatsController {
+    private final static int maxPoint = 10;
+    private final static int minPoint = 5;
+    private final static int plusMinus = 1;
+
+    @Override
+    public int increaseAtt(int attack) {
+        if (attack < maxPoint) {
+            attack += plusMinus;
+        } else {
+            System.err.println("Max Point Reached");
+        }
+        return attack;
+    }
+
+    @Override
+    public int increaseDef(int defence) {
+        if (defence < maxPoint) {
+            defence += plusMinus;
+        } else {
+            System.err.println("Max Point Reached");
+        }
+        return defence;
+    }
+
+    @Override
+    public int increaseSpe(int speed) {
+        if (speed < maxPoint) {
+            speed += plusMinus;
+        } else {
+            System.err.println("Max Point Reached");
+        }
+        return speed;
+    }
+
+    @Override
+    public int decreaseAtt(int attack) {
+        if (attack > minPoint) {
+            attack -= plusMinus;
+        } else {
+            System.err.println("Min Point Reached");
+        }
+        return attack;
+    }
+
+    @Override
+    public int decreaseDef(int defence) {
+        if (defence > minPoint) {
+            defence -= plusMinus;
+        } else {
+            System.err.println("Min Point Reached");
+        }
+        return defence;
+    }
+
+    @Override
+    public int decreaseSpe(int speed) {
+        if (speed > minPoint) {
+            speed -= plusMinus;
+        } else {
+            System.err.println("Min Point Reached");
+        }
+        return speed;
+    }
+
+}
