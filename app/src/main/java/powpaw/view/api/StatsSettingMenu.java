30df63e506f0d6c1040498cdf7bd310dbcff71a3
Simone Collorà <simone.collora@studio.unibo.it>
2023-03-21 12:36:39 +0100

    Prototype of StatsMenuSetting

--- app/src/main/java/powpaw/view/api/StatsSettingMenu.java
+++ app/src/main/java/powpaw/view/api/StatsSettingMenu.java
@@ -4,6 +4,9 @@ import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import powpaw.controller.api.StatsController;
+import powpaw.controller.impl.StatsControllerImpl;
+import powpaw.model.impl.StatsImpl;
 
 public class StatsSettingMenu extends GridPane {
     private final Button plusAtt;
@@ -14,7 +17,11 @@ public class StatsSettingMenu extends GridPane {
     private final Button minDef;
     private final Button minSpe;
     private final Button minDex;
-    
+    private final Button finish;
+
+    private StatsImpl stats = new StatsImpl();
+    private StatsController control = new StatsControllerImpl();
+
     private final Button exit;
 
     public StatsSettingMenu() {
@@ -27,6 +34,8 @@ public class StatsSettingMenu extends GridPane {
         minSpe = new Button("-");
         minDex = new Button("-");
         exit = new Button("MENU");
+        finish = new Button("Finish");
+        setAlignment(Pos.CENTER);
         setVgap(15);
         setHgap(15);
         plusAtt.prefWidthProperty().bind(widthProperty().divide(5));
@@ -36,11 +45,36 @@ public class StatsSettingMenu extends GridPane {
         plusAtt.setMaxSize(50, 50);
         exit.setMaxSize(50, 50);
         add(plusAtt, 0, 0);
-        plusAtt.setScaleX(BASELINE_OFFSET_SAME_AS_HEIGHT);
         add(minAtt, 0, 1);
         add(exit, 10, 10);
+        add(finish,0,10);
+        add(plusDef,5,5);
         exit.setOnAction(e -> {
             exit.getScene().setRoot(new StartMenu());
+        });
+        plusAtt.setOnAction(e -> {
+            System.out.println(stats.getAttack());
+            stats = new StatsImpl.StatsBuilder().attack(control.increaseAtt(stats.getAttack()))
+            .defence(stats.getDefence()).speed(stats.getSpeed())
+            .build();
+            System.out.println(stats.getAttack());
+        });
+        minAtt.setOnAction(e -> {
+            System.out.println(stats.getAttack());
+            stats = new StatsImpl.StatsBuilder().attack(control.decreaseAtt(stats.getAttack()))
+                    .defence(stats.getDefence()).speed(stats.getSpeed())
+                    .build();
+            System.out.println(stats.getAttack());
+        });
+        plusDef.setOnAction(e -> {
+            System.out.println(stats.getDefence());
+            stats = new StatsImpl.StatsBuilder().attack(stats.getAttack())
+            .defence(control.increaseDef(stats.getDefence())).speed(stats.getSpeed())
+            .build();
+            System.out.println(stats.getDefence());
+        });
+        finish.setOnAction(e ->{
+            System.out.println("A: "+ stats.getAttack() +" D:"+ stats.getDefence() + " S:"+ stats.getSpeed());
         });
     }
 }
