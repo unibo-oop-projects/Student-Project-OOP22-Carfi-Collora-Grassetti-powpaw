92a6a5f56c8ea1480cb936b0f8409bc81861e55e
Simone Collorà <simone.collora@studio.unibo.it>
2023-03-22 16:08:56 +0100

    Damage double to string

--- app/src/main/java/powpaw/view/api/GameInterface.java
+++ app/src/main/java/powpaw/view/api/GameInterface.java
@@ -4,16 +4,18 @@ import javafx.geometry.Pos;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import powpaw.model.impl.DamageMeterImpl;
 
 public class GameInterface extends StackPane {
-    private double damageP1 = 0.0;
-    private double damageP2 = 0.0;
+    DamageMeterImpl damage = new DamageMeterImpl();
+    private String p1 = damage.toString();
+    private String p2 = damage.toString();
     private Text damP1;
     private Text damP2;
 
     public GameInterface() {
-        damP1 = new Text(0, 0, "P1: " + String.valueOf(damageP1));
-        damP2 = new Text(0, 0, "P2: " + String.valueOf(damageP2));
+        damP1 = new Text(0, 0, "P1: " + p1);
+        damP2 = new Text(0, 0, "P2: " + p2);
         damP1.setStyle("-fx-font: 50 arial;");
         damP2.setStyle("-fx-font: 50 arial;");
         damP1.setFill(Color.RED);
