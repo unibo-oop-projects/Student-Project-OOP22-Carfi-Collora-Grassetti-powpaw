e1810080140d0f686448b6b5398c11f276cd7eca
alessia-carfi <alessia.carfi@studio.unibo.it>
2023-04-01 16:10:00 +0200

    fixed showGame method in GameStateViewImpl

--- app/src/main/java/powpaw/view/impl/GameStateViewImpl.java
+++ app/src/main/java/powpaw/view/impl/GameStateViewImpl.java
@@ -11,25 +11,26 @@ import powpaw.view.api.StartMenu;
 
 public class GameStateViewImpl implements GameStateView {
 
-
     Stage stage = new Stage();
     GameStateController gameStateController = new GameStateControllerImpl();
-    
+
+    public GameStateViewImpl() {
+        stage.setTitle("PowPaw");
+        stage.setResizable(false);
+
+    }
+
     @Override
     public void showStartMenu() {
         gameStateController.start();
-        stage.setScene(new Scene(new StartMenu(),ScreenController.SIZE_HD_W, ScreenController.SIZE_HD_H));
-        stage.setTitle("PowPaw");
-        stage.setResizable(false);
+        stage.setScene(new Scene(new StartMenu(), ScreenController.SIZE_HD_W, ScreenController.SIZE_HD_H));
         stage.show();
     }
 
     @Override
     public void showCharacterCreation() {
         gameStateController.characterCreation();
-        stage.setScene(new Scene(new StatsSettingMenu(),ScreenController.SIZE_HD_W, ScreenController.SIZE_HD_H));
-        stage.setTitle("PowPaw");
-        stage.setResizable(false);
+        stage.setScene(new Scene(new StatsSettingMenu(), ScreenController.SIZE_HD_W, ScreenController.SIZE_HD_H));
         stage.show();
     }
 
@@ -39,13 +40,10 @@ public class GameStateViewImpl implements GameStateView {
         WorldRenderImpl worldRender = new WorldRenderImpl();
         GameLoop loop = new GameLoop();
         stage.setScene(worldRender.render());
-        stage.setTitle("PowPaw");
-        stage.setResizable(false);
         stage.show();
-        worldRender.setKeyCommands();
+        worldRender.playersCommands();
         loop.setPlayerController(worldRender.getPlayerController());
         loop.setWeaponController(worldRender.getWeaponController());
-        loop.setPowerUpController(worldRender.getPowerUpController());
         loop.start();
     }
 
