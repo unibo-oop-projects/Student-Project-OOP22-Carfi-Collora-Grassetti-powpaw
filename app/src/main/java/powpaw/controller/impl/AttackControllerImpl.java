513b6a37486c39033bd42fa1964201782aa2abb3
Simone Collorà <simone.collora@studio.unibo.it>
2023-04-06 10:35:30 +0200

    Fixes

--- app/src/main/java/powpaw/controller/impl/AttackControllerImpl.java
+++ app/src/main/java/powpaw/controller/impl/AttackControllerImpl.java
@@ -19,9 +19,9 @@ public class AttackControllerImpl {
 
     // nel PlayerImpl
     public Optional<Player> checkHealtStatus() {
-        if (playerOne.getCurrentHealth().getDamage() >= MAX_PERC) {
+        if (playerOne.getCurrentHealth() >= MAX_PERC) {
             return Optional.of(this.playerOne);
-        } else if (playerOne.getCurrentHealth().getDamage() >= MAX_PERC) {
+        } else if (playerOne.getCurrentHealth() >= MAX_PERC) {
             return Optional.of(this.playerTwo);
         }
         return Optional.empty();
