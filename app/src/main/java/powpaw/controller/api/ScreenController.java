de074e5a68ec7eb2cce613efd5562be696836aa8
Giacomo Grassetti <giacomo.grassetti@studio.unibo.it>
2023-04-08 21:45:41 +0200

    add more javadoc

--- app/src/main/java/powpaw/controller/api/ScreenController.java
+++ app/src/main/java/powpaw/controller/api/ScreenController.java
@@ -2,15 +2,24 @@ package powpaw.controller.api;
 
 import powpaw.model.api.Hitbox;
 
+/**
+ * Static class contains a method to check if a given Hitbox object is out of the screen
+ * bounds.
+ * 
+ * @author Giacomo Grassetti
+ */
 public class ScreenController {
 
     public static final int SIZE_HD_W = 1280;
     public static final int SIZE_HD_H = 720;
 
-    public void setScreenSize(int width, int height) {
-        // TODO
-    }
-
+    
+    /**
+     * Method that checks if a given hitbox is out of the screen boundaries.
+     * 
+     * @param hitbox A Hitbox representing the position and size of an object in a game.
+     * @return True if the given Hitbox position is out of the screen, false otherwise.
+     */
     public static boolean isOutOfScreen(Hitbox hitbox) {
         if (hitbox.getCenter().getX() >= SIZE_HD_W
                 || hitbox.getCenter().getY() <= -SIZE_HD_H / 10) { // margine alto - destro
